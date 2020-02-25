cea30fa1f794633ecf47bfdb48bd928bb8f0ad91
Joy van der Veeken <jihveeken@gmail.com>
2020-02-25 12:16:29 +0000

    Added Web Font Loader to decrease load time Removed a fontFamily tag on hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = ({ heroText }) => {
     return (
         <Flex width='100vw' height='100vh' mt={{ default: '0', s: '-100vh' }} >
             <Box margin='auto' width={{ default: '60vw', l: '40vw' }} flexDirection='column' justifyContent='center'>
-                <H1 fontFamily='Lato' color='#FFF' fontSize={{ default: '1.2em', s: '2em' }}  >{heroText}</H1>
+                <H1 color='#FFF' fontSize={{ default: '1.2em', s: '2em' }}  >{heroText}</H1>
             </Box>
         </Flex>
     )
